386de1b0f82993c4945c977bf43694bd78e290ea
iMarishin <move_up@icloud.com>
2021-08-03 17:42:17 +0500

    finish 1 path

--- HellowApp/Registration/TextRegView.swift
+++ HellowApp/Registration/TextRegView.swift
@@ -28,6 +28,7 @@ struct TextRegView: View {
         }
         .frame(width: 315, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
         .overlay(Capsule().stroke(AngularGradient(gradient: user.gradient, center: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/), lineWidth: 8))
+        .shadow(color: Color(.systemBlue), radius: 5, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/)
     }
 }
 
--- HellowApp/Timer/ButtonTimerView.swift
+++ HellowApp/Timer/ButtonTimerView.swift
@@ -9,17 +9,26 @@ import SwiftUI
 
 struct ButtonTimerView: View {
     @Binding var title: String
+    var gradient: Gradient
     var action: () -> Void
     var cleanAction: () -> Void
     
     var body: some View {
         HStack {
             Button(action: action, label: {
-                    Text(title)
+                Text(title)
+                    .foregroundColor(Color.black)
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .bold()
             })
+            .frame(width: 200, height: 60, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            .background(LinearGradient(gradient: gradient, startPoint: .topLeading, endPoint: .bottomTrailing))
+            .cornerRadius(25)
+            .shadow(color: Color(.systemBlue), radius: 5, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/)
             Spacer()
             Button(action: cleanAction, label: {
                 Text("Logout")
+                    .bold()
             })
         }
         .padding()
