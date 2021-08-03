c81bda330f9651b78b1b3ade55ce6666ca88e7db
iMarishin <move_up@icloud.com>
2021-08-03 12:55:23 +0500

    Initial Commit

---
+++ HellowApp/HellowAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HellowAppApp.swift
+//  HellowApp
+//
+//  Created by Иван Маришин on 03.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct HellowAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
